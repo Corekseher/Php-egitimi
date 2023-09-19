72d020e877b4aec2b873e569feef77da737fde93
seher çörekoğlu <139799560+seheerr@users.noreply.github.com>
2023-09-19 20:36:14 +0300

    Add files via upload

---
+++ phpegitimi17/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>php eğitim</title>
+</head>
+
+<body>
+   <?php
+  $Deger["PHPegitimi"] = "merhab";
+  $Deger["HTMLegitimi"]  = "nasilsin";
+  $Deger["CSSegitimi"] = "iyigunler" ;
+
+  echo "<pre>";
+  print_r($Deger);
+  echo "</pre>";
+   
+   ?>
+</body>
+</html>
