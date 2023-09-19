6f69d1cbf698f1383a72c5d6af9b5d3926682b82
seher çörekoğlu <139799560+seheerr@users.noreply.github.com>
2023-09-19 20:35:56 +0300

    Add files via upload

---
+++ phpegitimi - Kopya16/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="tr-TR">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta http-equiv="Content-Language" content="tr">
+<meta charset="utf-8">
+<title>php eğitim</title>
+</head>
+
+<body>
+   <?php
+  $DegerBir  = "merhab";
+  $DegerIki   = "nasilsin";
+  define("SONUC",$DegerBir."-".$DegerIki);
+  echo SONUC;
+   
+   ?>
+</body>
+</html>
